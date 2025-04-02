d5dff047aec593eb901518ef191955ce5bd9b349
joaopbarcellos <jpzbarcellos@gmail.com>
2025-04-02 16:52:39 -0300

    ajustando tela de resolucao para tailwind

--- modular/modules/Resolucao/components/ResolucaoTable.vue
+++ modular/modules/Resolucao/components/ResolucaoTable.vue
@@ -1,8 +1,29 @@
 <script setup lang="ts">
 import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+dayjs.extend(utc);
 import { useResolucoes } from '../composables/useViewResolucaoPage';
+import SvgIcon from '@jamescoyle/vue-icon';
+import { mdiPageFirst, mdiPageLast, mdiChevronLeft, mdiChevronRight, mdiArrowUp, mdiArrowDown, mdiEye } from '@mdi/js';
 
-const { header, filterType, filterTypes, resolucoes } = useResolucoes();
+const { 
+  header, 
+  filterType, 
+  filterTypes, 
+  resolucoes, 
+  sortTable, 
+  sortKey, 
+  sortDirection, 
+  loadResolucoes, 
+  paginatedData, 
+  currentPage, 
+  pageSize, 
+  pageSizes, 
+  totalPages, 
+  searchQuery,
+  buscar,
+  filterLabel
+  } = useResolucoes();
 </script>
 
 <template>
@@ -21,14 +42,115 @@ const { header, filterType, filterTypes, resolucoes } = useResolucoes();
     </v-col>
   </v-row>
 
-  <v-row>
-    <v-col cols="4">
-      <v-select v-model="filterType" :items="filterTypes" label="Filtrar por Status" outlined dense />
-    </v-col>
-  </v-row>
+  <div class="flex items-center w-full mt-6 mb-10">
+    <select v-model="filterType" class="h-12 rounded border px-2 ml-2 text-sm focus:outline-none focus:ring-2 focus:ring-blue-800">
+      <option v-for="option in filterTypes" :value="option">
+        {{ option }}
+      </option>
+    </select>
+    <input
+      v-model="searchQuery"
+      type="text"
+      :placeholder="filterLabel"
+    />
+    <button
+      class="border border-gray-300 flex justify-end"
+      @click="buscar"
+      >Buscar
+    </button>
+  </div>
+  <div >
+    <div class="w-full">
+      <table class="w-full">
+          <thead>
+            <tr class="border-b border-gray-200 bg-gray-50">
+              <th
+                v-for="(header, index) in header"
+                :key="index"
+                @click="sortTable(header.key)"
+                :class="{
+                  'px-7 py-3 text-left text-sm font-semibold text-gray-600': true, 
+                  'cursor-default': header.key === 'actions' || header.key === 'ementa', 
+                  'hover:cursor-pointer': header.key !== 'actions' && header.key !== 'ementa',
+                }"
+              > 
+                <div class="flex items-center">
+                {{ header.title }}
+                <span v-if="sortKey === header.key" class="ml-1">
+                  <svg-icon class="me-2" :size='20' :path="mdiArrowUp" type="mdi" v-if="sortDirection === 'asc'"></svg-icon>
+                  <svg-icon class="me-2" :size='20' :path="mdiArrowDown" type="mdi" v-else-if="sortDirection === 'desc'"></svg-icon>
+                </span>
+              </div>
+              </th>
+            </tr>
+          </thead>
 
-  <v-data-table :headers="header" :items="resolucoes" :items-per-page="10">
-    <template #bottom></template>
-  </v-data-table>
+          <tbody>
+            <tr v-for="item in paginatedData" :key="item.Numero" class="hover:bg-gray-50 border-b border-gray-200">
+              <td class="px-7 py-3 text-sm text-gray-700 whitespace-nowrap ">
+                <a href="#" @click.prevent="abrirDialogBolsista(item)" style="color: #2B3A8A;">
+                    {{ item.Numero }}
+                </a>
+              </td>
+              <td class="px-7 py-3 text-sm font-semibold text-gray-900 whitespace-nowrap max-w-[800px]">
+                <span>{{ item.Ementa }}</span>
+              </td>
+              <td class="px-7 py-3 text-sm text-gray-700 whitespace-nowrap">{{ dayjs.utc(item.Data).format('DD/MM/YYYY')  }}</td>
+              <td class="px-7 py-3 text-sm text-gray-700 whitespace-nowrap">
+                <svg-icon class="me-2" style="color: #2B3A8A" :size='22' :path="mdiEye" type="mdi" dark @click.prevent="abrirDialogBolsista(item)"></svg-icon>
+              </td>
+            </tr>
+          </tbody>
+      </table>
+    </div>
+    <div class="mt-4 text-right flex items-center justify-end">
+      <span class="whitespace-nowrap text-sm">Itens por página</span>
+      <select 
+        v-model="pageSize"
+        class="h-8 rounded border px-2 ml-2 text-sm focus:outline-none focus:ring-1 focus:ring-blue-500"
+      >
+        <option v-for="size in pageSizes" :key="size" :value="size">
+          {{ size }}
+        </option>
+      </select>
+
+      <button 
+        :disabled="currentPage === 1"
+        @click="currentPage = 1"  
+        class="h-8 w-8 rounded hover:bg-gray-100 disabled:opacity-50 ml-6"
+      >
+        <svg-icon class="mb-1" :path="mdiPageFirst" type="mdi"></svg-icon>
+      </button>
+      
+      
+      <button
+        @click="currentPage--"
+        :disabled="currentPage === 1"
+        class="h-8 w-8 rounded hover:bg-gray-100 disabled:opacity-50"
+      >
+        <svg-icon class="mb-1" :path="mdiChevronLeft" type="mdi"></svg-icon>
+      </button>
+      
+      <span class="px-2 text-sm">Página {{ currentPage }} de {{ totalPages? totalPages : 1 }}</span>
+      
+      <button
+        @click="currentPage++"
+        :disabled="currentPage === totalPages"
+        class="h-8 w-8 rounded hover:bg-gray-100 disabled:opacity-50"
+      >
+        
+        <svg-icon class="mb-1" :path="mdiChevronRight" type="mdi"></svg-icon>
+      </button>
+      
+      <button
+        @click="currentPage = totalPages"
+        :disabled="currentPage === totalPages"
+        class="h-8 w-8 rounded hover:bg-gray-100 disabled:opacity-50"
+      >
+        <svg-icon class="mb-1" :path="mdiPageLast" type="mdi"></svg-icon>
+      </button>
+      
+    </div>
+  </div>
 </template>
 
--- modular/modules/Resolucao/composables/useViewResolucaoPage.ts
+++ modular/modules/Resolucao/composables/useViewResolucaoPage.ts
@@ -1,37 +1,125 @@
-import { inject, onMounted, ref } from 'vue';
+import { inject, onMounted, ref, computed } from 'vue';
 import type { IResolucao } from '../api/interface';
 import { resolucaoService } from '../api/factory'; // modular\modules\Resolucao\api\factory.ts
 
 export function useResolucoes() {
 
   const header = ref([
-    { title: 'Numero', sortable: false, key: 'Numero' },
-    { title: 'Data de Criação', sortable: false, key: 'Data' },
-    { title: 'Ementa', sortable: false, key: 'Ementa' },
-    { title: 'Link', sortable: false, key: 'Link' },
-    { title: 'Número do E-Docs', sortable: false, key: 'NumRastreioEdocs' }
+    { title: 'Número', sortable: false, key: 'numero' },
+    { title: 'Ementa', sortable: false, key: 'ementa' },
+    { title: 'Publicação', sortable: false, key: 'publicacao' },
+    { title: 'Ações', sortable: false, key: 'actions' }
   ]);
 
   const filterType = ref('');
-  const filterTypes = ref([
-    { text: 'Todos', value: '' },
-    { text: 'Ativos', value: 'ativo' },
-    { text: 'Inativos', value: 'inativo' }
-  ]);
-
+  const filterTypes = ref(['Número da Resolução', 'Ementa']);
+  const searchQuery = ref('');
   const resolucoes = ref<IResolucao[]>([]);
+  const filteredResolucoes = ref<IResolucao[]>([]);
 
   const loadResolucoes = async () => {
     try {
       const response = await resolucaoService.getResolucao();
       console.log('Dados recebidos:', response);
       resolucoes.value = response?.value || [];
+      filteredResolucoes.value = response?.value || [];
     } catch (error) {
       console.error('Erro ao buscar resoluções:', error);
     }
   };
 
+  const sortKey = ref('');
+  const sortDirection = ref('asc');
+
+  const filterLabel = computed(() => {
+    if (filterType.value === 'Ementa') {
+      return 'Filtrar por Ementa';
+    }
+    if (filterType.value === 'Número da Resolução') {
+      return 'Filtrar por Número da Resolução';
+    }
+    else {
+      return 'Pesquise por palavra-chave';
+    }
+  });
+
+  function sortTable(key: any) {
+      if (key === 'ementa' || key === 'actions') {
+          return
+      }
+      if (sortKey.value === key) {
+          sortDirection.value = sortDirection.value === 'asc' ? 'desc' : 'asc';
+      } else {
+          sortKey.value = key;
+          sortDirection.value = 'asc';
+      }
+  }
+
+  const sortedData = computed(() => {
+      return [...filteredResolucoes.value].sort((a: any, b: any) => {
+          let modifier = sortDirection.value === 'asc' ? 1 : -1;
+          return a[sortKey.value] > b[sortKey.value] ? modifier : -modifier;
+      }).filter((item) => {
+        if (filterType.value === ""){
+            return item
+        }
+        if (filterType.value === 'Número da Resolução') {
+          return item.Numero == searchQuery.value;
+        }
+        return item.Ementa.toLowerCase().includes(searchQuery.value.toLowerCase());
+      });
+  });
+
+  const currentPage = ref(1);
+  const pageSize = ref(10);
+  const pageSizes = ref([5, 10, 20, 50]);
+
+  const paginatedData = computed(() => {
+    const start = (currentPage.value - 1) * pageSize.value;
+    const end = start + pageSize.value;
+    return sortedData.value.slice(start, end);
+  });
+
+  const totalPages = computed(() => {
+    return Math.ceil(sortedData.value.length / pageSize.value);
+  });
+
+
+  const buscar = () => {
+    if (!searchQuery.value) {
+      filteredResolucoes.value = resolucoes.value;
+    } else {
+      const query = searchQuery.value.toLowerCase();
+      filteredResolucoes.value = resolucoes.value.filter((item) => {
+          if (filterType.value === 'Número da Resolução') {
+            return item.Numero == query;
+          } else if (filterType.value === 'Ementa') {
+            return item.Ementa.toLowerCase().includes(query);
+          } else {
+            return item.Ementa.toLowerCase().includes(query) || item.Numero == query;
+          }
+      });
+    }
+  }
+
   onMounted(loadResolucoes);
 
-  return { header, filterType, filterTypes, resolucoes };
+  return { 
+    header, 
+    filterType, 
+    filterTypes, 
+    resolucoes, 
+    sortTable, 
+    sortKey, 
+    sortDirection, 
+    loadResolucoes, 
+    paginatedData, 
+    currentPage, 
+    pageSize, 
+    pageSizes, 
+    totalPages, 
+    searchQuery,
+    buscar,
+    filterLabel
+  };
 }
